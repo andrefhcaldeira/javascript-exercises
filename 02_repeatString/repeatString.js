eb6f6fcf935b7fe0f5851b60c11b60a746d9fffa
andrefhcaldeira <andrefhcaldeira@proton.me>
2023-09-22 11:28:53 +0100

    Solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, repeatTimes) {
+    if (repeatTimes < 0) {
+        return 'ERROR';
+    }
+    let string = '';
+    for (let i = 0; i < repeatTimes; i++) {
+        string += repeatString;
+    }
+    return string;
 };
 
 // Do not edit below this line
