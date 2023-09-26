5b8476d64d09db9938482fb9a28f8389654c3c58
andrefhcaldeira <andrefhcaldeira@proton.me>
2023-09-26 10:09:32 +0100

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let string = '';
+    // for each character in phrase
+    // string + character
+    // print string
+    for (let i = phrase.length; i != -1; i--) {
+        string += phrase.charAt(i);
+    }
+    return string;
 };
 
 // Do not edit below this line
