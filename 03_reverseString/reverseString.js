53d1452414b36356784c444a7b0f65094a110cf2
andrefhcaldeira <andrefhcaldeira@proton.me>
2023-09-26 10:10:09 +0100

    Removed unnecessary comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,5 @@
 const reverseString = function(phrase) {
     let string = '';
-    // for each character in phrase
-    // string + character
-    // print string
     for (let i = phrase.length; i != -1; i--) {
         string += phrase.charAt(i);
     }
