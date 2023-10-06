19a712bf5e42668bd227bb3554e5b0bd45bd1eb1
andrefhcaldeira <andrefhcaldeira@proton.me>
2023-10-06 17:33:36 +0100

    Small exercise 05 change

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,6 @@
 const sumAll = function(first, last) {
     let total = 0;
-    if (first > 0 &&
-        last > 0 &&
-        Number.isInteger(last) === true &&
-        Number.isInteger(first) === true) 
-        {
+    if (first > 0 && last > 0 && Number.isInteger(last) === true && Number.isInteger(first) === true) {
         if (first < last) {
             for (let i = first; i < (last + 1); i++) {
                 total = total + i;
