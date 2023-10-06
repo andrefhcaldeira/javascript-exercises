db4aed6ae1d0d13a71caf215a3277a771cd5196c
andrefhcaldeira <andrefhcaldeira@proton.me>
2023-10-06 17:30:50 +0100

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let total = 0;
+    if (first > 0 &&
+        last > 0 &&
+        Number.isInteger(last) === true &&
+        Number.isInteger(first) === true) 
+        {
+        if (first < last) {
+            for (let i = first; i < (last + 1); i++) {
+                total = total + i;
+            }
+        } else {
+            for (let i = last; i < (first + 1); i++) {
+                total = total + i;
+            }
+        }
+        return total;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
